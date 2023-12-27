e369ed439f3d7c0ed55886c909f9636c50f0cbf7
Priyanshi Maheshwari <priyanshimaheshwari2015@gmail.com>
2023-12-27 14:21:51 +0530

    Home page and main layout added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/Layouts/MainLayout';
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+        Hello
+    </MainLayout>
+  )
+}
+
+export default HomePage;
