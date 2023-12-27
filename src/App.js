3a48073e9b6ad292c4c54a2979cfa1dc497db9d1
Priyanshi Maheshwari <priyanshimaheshwari2015@gmail.com>
2023-12-27 13:20:55 +0530

    Onboarding Stepper Page Added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import DashboardPage from './pages/DashboardPage';
 import ProtectedRoute from './components/common/ProtectRoute';
 import { AuthProvider } from './utils/authContext';
 import TokenHandler from './pages/TokenHandler';
+import OnboardingPage from './pages/OnboardingPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<LoginPage />} />
           <Route path="/token-handler" element={<TokenHandler></TokenHandler>} />
-          <Route path="/dashboard" element={<ProtectedRoute component={DashboardPage}></ProtectedRoute>} />
+          {/* <Route path="/dashboard" element={<ProtectedRoute component={DashboardPage}></ProtectedRoute>} /> */}
+          <Route path="/onboarding" element={<ProtectedRoute component={OnboardingPage}></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute component={HomePage}></ProtectedRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
