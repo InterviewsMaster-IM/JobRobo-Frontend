43bc01b2fa0314e6295230d7f07059feaa4d4da8
Priyanshi Maheshwari <priyanshimaheshwari2015@gmail.com>
2023-12-27 14:16:38 +0530

    Constants file and logo image added

---
+++ src/utils/Constants.js
@@ -0,0 +1,90 @@
+export const raceOptionsList = [
+    {
+        label: 'Hispanic or Latino',
+        value: 'hispanic_latino'
+    },
+    {
+        label: 'American Indian or Alaska Native',
+        value: 'americanIndian_alaskaNative'
+    },
+    {
+        label: 'Asian',
+        value: 'asian'
+    },
+    {
+        label: 'Black or African American',
+        value: 'black_africanAmerican'
+    },
+    {
+        label: 'Native Hawaiian or Other Pacific Islander',
+        value: 'hawaiian'
+    },
+    {
+        label: 'Two or More Races',
+        value: 'two_or_more'
+    },
+    {
+        label: 'White',
+        value: 'white'
+    },
+    {
+        label: 'Prefer not to specify',
+        value: 'none'
+    },
+]
+
+export const veteranStatusOptions = [
+    {
+        label: 'I am not a protected veteran',
+        value: 'no'
+    },
+    {
+        label: 'I identify as one or more of the classifications of protected veteran listed above',
+        value: 'yes'
+    },
+    {
+        label: 'Prefer not to specify',
+        value: 'none'
+    },
+]
+
+export const disabilityStatusOptions = [
+    {
+        label: 'Yes, I have a disability, or have had one in the past',
+        value: 'yes'
+    },
+    {
+        label: 'No, I do not have a disability and have not had one in the past',
+        value: 'no'
+    },
+    {
+        label: 'Prefer not to answer',
+        value: 'none'
+    },
+]
+
+export const workPreferenceOptions = [
+    {
+        label: 'Onsite',
+        value: 'onsite'
+    },
+    {
+        label: 'Hybrid',
+        value: 'hybrid'
+    },
+    {
+        label: 'Remote',
+        value: 'remote'
+    },
+]
+
+export const booleanValues = [
+    {
+        label: 'Yes',
+        value: 'yes'
+    },
+    {
+        label: 'No',
+        value: 'no'
+    },
+]
