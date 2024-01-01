c7439e4d023b00e1bfa4406ef6a8c3d68819b700
Priyanshi Maheshwari <priyanshimaheshwari2015@gmail.com>
2024-01-01 23:11:29 +0530

    Material ui theme updates (#10)

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,42 +3,40 @@ import { createTheme, alpha } from '@mui/material/styles';
 const theme = createTheme({
     palette: {
         customGreen: {
-            base: '#55B982',
+            main: '#55B982',
             light: alpha('#55B982', 0.10),
             lighter: alpha('#EAF7EF', 0.30),
             dark: '',
             darker: '',
         },
         black: {
-            base: '#001405',
+            main: '#001405',
             light: '',
             lighter: '',
             dark: '',
             darker: '',
         },
         white: {
-            base: '#FFF',
+            main: '#FFF',
             light: '',
             dark: '',
         },
         grey: {
-            base: '#7F8781',
+            main: '#7F8781',
             lighter: '',
             light: '',
             dark: '',
             darker: ''
         },
-        error: {
-
-        },
         borderColors: {
-            green: alpha('#55B982', 0.30),
+            main: alpha('#55B982', 0.30),
             black: alpha('#001405', 0.10),
             grey: '#E5E5E5',
         },
     },
     typography: {
         fontFamily: 'Manrope, sans-serif',
+        color: '#001405',
     },
 });
 
