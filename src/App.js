11d0f361918912150353847ac7f17e527551c3c1
AshishBhalala11 <ashishbhalala2911@gmail.com>
2024-01-04 20:27:47 +0530

    account setting page cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import HomePage from './pages/HomePage';
 import PastJobRoboPage from './pages/PastJobRoboPage';
 import AppliedJobDetails from './components/PastJobRobos/AppliedJobDetails';
 import ProfilePage from './pages/ProfilePage';
-import ResumeQuestionsTests from './pages/resumeQuestionsTest';import AccountSettings from './pages/AccountSettings';
+import AccountSettingsPage from './pages/AccountSettingsPage';
+import ResumeQuestionsTests from './pages/resumeQuestionsTest';
 
 function App() {
     return (
@@ -25,7 +26,7 @@ function App() {
                     <Route path="/pastjobrobo" element={<ProtectedRoute component={PastJobRoboPage}></ProtectedRoute>} />
                     <Route path="/pastjobrobo/:id" element={<ProtectedRoute component={AppliedJobDetails}></ProtectedRoute>} />
                     <Route path="/profile" element={<ProtectedRoute component={ProfilePage}></ProtectedRoute>} />
-                    <Route path="/settings" element={<ProtectedRoute component={AccountSettings}></ProtectedRoute>}  />
+                    <Route path="/settings" element={<ProtectedRoute component={AccountSettingsPage}></ProtectedRoute>}  />
                     <Route path="/test" element={<ProtectedRoute component={ResumeQuestionsTests}></ProtectedRoute>} />
                 </Routes>
             </Router>
---
+++ src/pages/AccountSettingsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../components/Layouts/MainLayout';
+import AccountSettings from '../components/AccountSettings';
+
+const AccountSettingsPage = () => {
+  return (
+    <MainLayout>
+        <AccountSettings />
+    </MainLayout>
+)
+}
+
+export default AccountSettingsPage;
