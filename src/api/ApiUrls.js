db043252be9e9df57b0e29ba198ed6a5664ba8f5
AshishBhalala11 <ashishbhalala2911@gmail.com>
2024-01-08 16:59:01 +0530

    Revert "API integration"
    This reverts commit b6ea71045f31d0827cfd95a988efb39add806e56.

--- src/api/ApiUrls.js
+++ src/api/ApiUrls.js
@@ -2,16 +2,7 @@ const ApiUrls = {
     USER_CREDITS: 'credits/user-credits/',
     CREDIT_PLANS: 'credits/credit-plans/',
     CREDIT_HISTORY: 'credits/credit-history/',
-
     PROFILE_OTHER_DETAILS: 'profiles/other-details/',
-
-    CREATE_REFERRAL: 'referrals/create/',
-    VALIDATE_REFERRAL: 'referrals/validate/',
-
-    RESUME_QA: 'resumes/qa/',
-    RESUME_UPLOAD: 'resumes/upload/',
-    RESUME_START_TASK: 'resumes/start-task/',
-    RESUME_CHECK_TASK: 'resumes/check-task/',
 }
 
 export default ApiUrls;
--- src/store/store.js
+++ src/store/store.js
@@ -2,23 +2,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { creditsApi } from '../api/creditsApi';
 import { profileApi } from '../api/profileApi';
-import { referralsApi } from '../api/referralsApi';
-import { resumesApi } from '../api/resumesApi';
 
 export const store = configureStore({
-	reducer: {
-		[creditsApi.reducerPath]: creditsApi.reducer,
-		[profileApi.reducerPath]: profileApi.reducer,
-		[resumesApi.reducerPath]: resumesApi.reducer,
-		[referralsApi.reducerPath]: referralsApi.reducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(
-			creditsApi.middleware,
-			profileApi.middleware,
-			resumesApi.middleware,
-			referralsApi.middleware,
-		),
+  reducer: {
+    [creditsApi.reducerPath]: creditsApi.reducer,
+    [profileApi.reducerPath]: profileApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      creditsApi.middleware,
+      profileApi.middleware,
+      ),
 });
 
 // Optionally export the hooks if needed
