3824465f070102feb4761789920f6120d4a1ab3e
tarunkota <tarunkota4@gmail.com>
2024-01-14 21:22:12 +0530

    Added token in cookie

--- src/pages/TokenHandler.js
+++ src/pages/TokenHandler.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/authContext';
+import Cookies from 'js-cookie';
 
 const TokenHandler = () => {
     const navigate = useNavigate();
@@ -15,6 +16,9 @@ const TokenHandler = () => {
             // Store the token - localStorage can be used for simplicity
             localStorage.setItem('access_token', access_token);
             localStorage.setItem('refresh_token', refresh_token);
+            // Store the tokens in cookies as well
+            Cookies.set('access_token', access_token, { expires: 7 }); // Expires in 7 days
+            Cookies.set('refresh_token', refresh_token, { expires: 7 }); // Expires in 7 days
             // Redirect to another page after successful token handling
             // console.log("navigating to dashboard");
             if (promocode) {
