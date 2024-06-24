aed34e5dd496d926612e3c20bf4d2fd937aee8b9
bharathsurya021 <bharathsurya021@gmail.com>
2024-06-24 20:06:40 +0530

    handle server error

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -9,22 +9,42 @@ import { useGetJobsQuery } from '../../api/jobPostsApi';
 const Jobs = ({ jobTitle, numberOfJobs }) => {
     const { data: jobsList, error: jobsError, isLoading: jobsLoading } = useGetJobsQuery({ job_titles: jobTitle?.split(',').map(title => title.trim()), start_date: '2024-06-13T19:37:44.323+00:00', no_of_jobs: numberOfJobs });
 
-    return (<>
-        {(jobsLoading) && <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            textAlign: 'center',
-            height: '100vh',
-        }}>
-            <Dots color='#55B982' size={'18'} />
-            <Typography variant='body2' textAlign={'center'} fontSize={'24px'} fontWeight={'600'} color={'#55B982'} letterSpacing={'1.2px'}>
-                Please Wait
-            </Typography>
-        </Box>
-        }
+    if (jobsLoading) {
+        return (
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                height: '100vh',
+            }}>
+                <Dots color='#55B982' size={'18'} />
+                <Typography variant='body2' textAlign={'center'} fontSize={'24px'} fontWeight={'600'} color={'#55B982'} letterSpacing={'1.2px'}>
+                    Please Wait
+                </Typography>
+            </Box>
+        );
+    }
 
+    if (jobsError) {
+        return (
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+                height: '100vh',
+            }}>
+                <Typography variant='body2' textAlign={'center'} fontSize={'24px'} fontWeight={'600'} color={'#F44336'} letterSpacing={'1.2px'}>
+                    Please refresh or try again later.
+                </Typography>
+            </Box>
+        );
+    }
+
+    return (
         <Box boxSizing={'border-box'} component={"main"} sx={{ flexGrow: 1 }} padding={'2rem'}>
             <Grid container alignItems={'center'} justifyContent={'space-between'} gap={'1rem'}>
                 <Grid item display={'flex'} flexDirection={'column'} alignItems={'flex-start'} gap={'4px'}>
@@ -40,7 +60,6 @@ const Jobs = ({ jobTitle, numberOfJobs }) => {
                 }
             </Box>
         </Box>
-    </>
     );
 }
 
