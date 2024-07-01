76a6842190b685e56079ea898a9139c0168ef96f
bharathsurya021 <bharathsurya021@gmail.com>
2024-07-01 11:29:00 +0530

    handl loading logic and error logic

--- src/pages/JobPostsPage.js
+++ src/pages/JobPostsPage.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import JobPosts from '../components/JobPosts';
 import { useGetPersonalInfoQuery } from '../api/personalInfoApi';
 import { useGetJobSearchFiltersQuery } from '../api/filtersApi';
 import { generateUuidForUserEmail } from '../utils/Helpers';
+import Box from '@mui/material/Box';
+import Dots from 'react-activity/dist/Dots';
+import Typography from '@mui/material/Typography';
+
 const JobPostsPage = () => {
-  const { data: personalDetail } = useGetPersonalInfoQuery();
+  const { data: personalDetail, isLoading: personalInfoLoading } = useGetPersonalInfoQuery();
   const [userId, setUserId] = useState(null);
   const [filtersData, setFiltersData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const generateUserId = async () => {
@@ -18,22 +23,45 @@ const JobPostsPage = () => {
     generateUserId();
   }, [personalDetail]);
 
-  const { data: filters } = useGetJobSearchFiltersQuery(userId, {
+  const { data: filters, isLoading: filtersLoading } = useGetJobSearchFiltersQuery(userId, {
     skip: !userId,
   });
 
   useEffect(() => {
     if (filters) {
       setFiltersData(filters);
+      setLoading(false);
     }
   }, [filters]);
+
+
+  if (personalInfoLoading || filtersLoading) {
+    return (
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        height: '100vh',
+      }}>
+        <Dots color='#55B982' size={'18'} />
+        <Typography variant='body2' textAlign={'center'} fontSize={'24px'} fontWeight={'600'} color={'#55B982'} letterSpacing={'1.2px'}>
+          Please Wait
+        </Typography>
+      </Box>
+    );
+  }
+
   return (
-    <JobPosts jobTitles={filtersData?.jobTitle}
+    <JobPosts
+      jobTitles={filtersData?.jobTitle}
       countryLocation={filtersData?.countryLocation}
       experienceYears={filtersData?.experienceYears}
       datePosted={filtersData?.datePosted}
-      numberOfJobs={filtersData?.numberOfJobs} />
-  )
+      numberOfJobs={filtersData?.numberOfJobs}
+    />
+  );
 }
 
 export default JobPostsPage;
--- src/pages/JobsPage.js
+++ src/pages/JobsPage.js
@@ -3,11 +3,15 @@ import Jobs from '../components/Jobs';
 import { useGetPersonalInfoQuery } from '../api/personalInfoApi';
 import { useGetJobSearchFiltersQuery } from '../api/filtersApi';
 import { generateUuidForUserEmail } from '../utils/Helpers';
+import Box from '@mui/material/Box';
+import Dots from 'react-activity/dist/Dots';
+import Typography from '@mui/material/Typography';
 
 const JobsPage = () => {
-  const { data: personalDetail } = useGetPersonalInfoQuery();
+  const { data: personalDetail, isLoading: personalInfoLoading } = useGetPersonalInfoQuery();
   const [userId, setUserId] = useState(null);
   const [filtersData, setFiltersData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const generateUserId = async () => {
@@ -19,17 +23,36 @@ const JobsPage = () => {
     generateUserId();
   }, [personalDetail]);
 
-  const { data: filters } = useGetJobSearchFiltersQuery(userId, {
+  const { data: filters, isLoading: filtersLoading } = useGetJobSearchFiltersQuery(userId, {
     skip: !userId,
   });
 
   useEffect(() => {
     if (filters) {
       setFiltersData(filters);
+      setLoading(false); // Set loading to false when filters are fetched
     }
   }, [filters]);
 
-  return <Jobs jobTitle={filtersData?.jobTitle} numberOfJobs={filtersData?.numberOfJobs} countryLocation={filtersData?.countryLocation} />;
+  if (personalInfoLoading || filtersLoading) {
+    return (
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        height: '100vh',
+      }}>
+        <Dots color='#55B982' size={'18'} />
+        <Typography variant='body2' textAlign={'center'} fontSize={'24px'} fontWeight={'600'} color={'#55B982'} letterSpacing={'1.2px'}>
+          Please Wait
+        </Typography>
+      </Box>
+    );
+  }
+
+  return <Jobs jobTitle={filtersData?.jobTitle} numberOfJobs={filtersData?.numberOfJobs} countryLocation={filtersData?.countryLocation} datePosted={filtersData?.datePosted} />;
 }
 
 export default JobsPage;
